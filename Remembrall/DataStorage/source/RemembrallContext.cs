c799e3df101277dac06f7655a2a7e5d1dc9ee9e9
ik-ctrl <kondratyev.ie@gmail.com>
2020-08-27 21:08:16 +0300

    добавил DbSets

--- Remembrall/DataStorage/source/RemembrallContext.cs
+++ Remembrall/DataStorage/source/RemembrallContext.cs
@@ -6,16 +6,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataStorage.source
 {
-    public class RemembrallContext:DbContext 
+    public class RemembrallContext : DbContext
     {
         public RemembrallContext()
         {
-        
+
         }
 
         public RemembrallContext(DbContextOptions options) : base(options)
         {
-            
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -25,14 +25,50 @@ namespace DataStorage.source
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            ConfigurationPeople(modelBuilder);
+            ConfigurationEmail(modelBuilder);
+            ConfigurationPhone(modelBuilder);
+            ConfigurationRelationship(modelBuilder);
+            ConfigurationNotes(modelBuilder);
             base.OnModelCreating(modelBuilder);
         }
 
-        public DbSet<Person>People { get; set; }
+
+
+        public DbSet<Person> People { get; set; }
         public DbSet<Email> Emails { get; set; }
         public DbSet<Phone> Phones { get; set; }
         public DbSet<Note> Notes { get; set; }
         public DbSet<Relationship> Relations { get; set; }
+
+
+        #region config region 
+        private void ConfigurationPhone(ModelBuilder builder)
+        {
+
+        }
+
+        private void ConfigurationEmail(ModelBuilder builder)
+        {
+
+        }
+
+        private void ConfigurationRelationship(ModelBuilder builder)
+        {
+
+        }
+
+        private void ConfigurationPeople(ModelBuilder builder)
+        {
+
+        }
+
+        private void ConfigurationNotes(ModelBuilder builder)
+        {
+
+        }
+        #endregion
+      
 
 
 
