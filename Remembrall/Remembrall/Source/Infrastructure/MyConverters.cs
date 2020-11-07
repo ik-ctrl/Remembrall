f9d14708068c3246aee49c45faf1b92dfd7175c1
ik-ctrl <kondratyev.ie@gmail.com>
2020-11-07 16:24:15 +0300

    добавил класс для  объеденения конверторов

---
+++ Remembrall/Remembrall/Source/Infrastructure/MyConverters.cs
@@ -0,0 +1,46 @@
+﻿namespace Remembrall.Source.Infrastructure
+{
+    public static class MyConverters
+    {
+        /// <summary>
+        /// Метод для конвертации номера месяца в название месяца
+        /// </summary>
+        /// <param name="month"></param>
+        /// <returns></returns>
+        public static string ConvertMonthToString(int month)
+        {
+            switch (month)
+            {
+                case 1:
+                    return "января";
+                case 2:
+                    return "февраля";
+                case 3:
+                    return "марта";
+                case 4:
+                    return "апреля";
+                case 5:
+                    return "мая";
+                case 6:
+                    return "июня";
+                case 7:
+                    return "июля";
+                case 8:
+                    return "августа";
+                case 9:
+                    return "сентября";
+                case 10:
+                    return "октября";
+                case 11:
+                    return "ноября";
+                case 12:
+                    return "января";
+                default:
+                {
+                    return "Unknow";
+                    break;
+                }
+            }
+        }
+    }
+}
--- Remembrall/Remembrall/Source/ViewModel/SpecialDateViewModel.cs
+++ Remembrall/Remembrall/Source/ViewModel/SpecialDateViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using DataStorage.Source.Entity;
+using Remembrall.Source.Infrastructure;
 
 namespace Remembrall.Source.ViewModel
 {
@@ -25,7 +26,7 @@ namespace Remembrall.Source.ViewModel
         /// <summary>
         /// Месяц
         /// </summary>
-        public string Month => ConvertMonthTostring(_month);
+        public string Month => MyConverters.ConvertMonthToString(_month);
 
         /// <summary>
         /// Описания праздника
@@ -37,46 +38,6 @@ namespace Remembrall.Source.ViewModel
         /// </summary>
         public string CurrentDayInfo => $"{_day} {Month} : {Description}";
 
-        /// <summary>
-        /// Метод для конвертации номера месяца в название месяца
-        /// </summary>
-        /// <param name="month"></param>
-        /// <returns></returns>
-        private string ConvertMonthTostring(int month)
-        {
-            switch (month)
-            {
-                case 1:
-                    return "января";
-                case 2:
-                    return "февраля";
-                case 3:
-                    return "марта";
-                case 4:
-                    return "апреля";
-                case 5:
-                    return "мая";
-                case 6:
-                    return "июня";
-                case 7:
-                    return "июля";
-                case 8:
-                    return "августа";
-                case 9:
-                    return "сентября";
-                case 10:
-                    return "октября";
-                case 11:
-                    return "ноября";
-                case 12:
-                    return "января";
-                default:
-                    {
-                        return "Unknow";
-                        break;
-                    }
-
-            }
-        }
+      
     }
 }
