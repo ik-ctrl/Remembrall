1d90a4564638a24231c0a181320caefde52446d2
ik-ctrl <kondratyev.ie@gmail.com>
2020-11-23 21:33:13 +0300

    добавил мессенджер

---
+++ Remembrall/Remembrall/Source/Infrastructure/Messanger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Remembrall.Source.Infrastructure
+{
+    public class Messanger
+    {
+        /// <summary>
+        /// Синголтон для хранения подписок
+        /// </summary>
+        private static NotifyMessage _notify;
+
+        public Messanger()
+        {
+            if (_notify == null)
+                _notify = new NotifyMessage();
+        }
+
+        /// <summary>
+        /// Добавление обработчиков события
+        /// </summary>
+        /// <param name="action"></param>
+        public void AddUpdater(Action action)
+        {
+            _notify.UpdateViewNotify += new NotifyMessage.UpdateHolidayView(action);
+        }
+
+        /// <summary>
+        /// Вызов обработчиков событий
+        /// </summary>
+        public void InvokeUpdate()
+        {
+            _notify?.InvokeToUpdateView();
+        }
+
+    }
+
+
+    
+}
